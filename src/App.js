cd6dfd9fd15da1d27a33ea844c504174b2e2c703
dionysus0214 <hiba0214@naver.com>
2023-03-05 15:09:27 +0900

    JSX 문법 활용

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'jeje';
+  const list = ['봄', '여름', '가을', '겨울'];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className='orange'>{`Hello! ${name}`}</h1>
+      <ul>
+        {list.map(item => <li>{item}</li>)}
+      </ul>
+      <img
+        style={{ width: '200px', height: '200px' }}
+        src='https://images.unsplash.com/photo-1667964394218-0759343685e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=1000&q=60'
+        alt=''
+      />
+    </>
   );
 }
 
