e3f9a32c7e17e7d25946405edcbb006345421b21
dionysus0214 <hiba0214@naver.com>
2023-03-08 22:10:21 +0900

    useState를 활용한 counter component 제작

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -2,8 +2,12 @@ import "./App.css";
 import Profile from "./components/Profile";
 
 function AppProfile() {
+  const handleEvent = (event) => {
+    alert("버튼이 클릭됨!");
+  };
   return (
     <>
+      <button onClick={handleEvent}>버튼</button>
       <Profile
         image="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80"
         name="Loa Kim"
