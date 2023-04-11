e6720d8ff724657c05574d8d28e029c38f1cc186
dionysus0214 <hiba0214@naver.com>
2023-04-11 21:50:37 +0900

    todo 프로젝트 분리 및 .prettierrc 설정

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
-import Button1 from './components/Button1';
-import Button2 from './components/Button2';
-import StyledComponent from './StyledComponent';
-import TailwindComponent from './TailwindComponent';
+import TodoList from './components/TodoList/TodoList';
 
 function App() {
   return (
     <>
-      <Button1 />
-      <Button2 />
-      <StyledComponent />
-      <TailwindComponent />
+      <TodoList />
     </>
   );
 }
