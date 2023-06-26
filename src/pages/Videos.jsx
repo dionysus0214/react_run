dff9be9ba704fd18f377fa90a9896a1eac61c0e1
dionysus0214 <hiba0214@naver.com>
2023-06-26 21:38:56 +0900

    ⚗️ Fake 활용

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
 import {useQuery} from '@tanstack/react-query';
-import axios from 'axios';
 import VideoCard from '../components/VideoCard';
+import FakeYoutube from '../api/fakeYoutube';
 
 export default function Videos() {
   const {keyword} = useParams();
@@ -10,10 +10,9 @@ export default function Videos() {
     isLoading,
     error,
     data: videos,
-  } = useQuery(['videos', keyword], async () => {
-    return axios
-      .get(`/videos/${keyword ? 'search' : 'popular'}.json`)
-      .then(res => res.data.items);
+  } = useQuery(['videos', keyword], () => {
+    const youtube = new FakeYoutube();
+    return youtube.search(keyword);
   });
   return (
     <>
